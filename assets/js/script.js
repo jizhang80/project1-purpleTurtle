b3af13587207dc7a8aed38859990198a800cba73
bestedt <ty_bested@hotmail.com>
2023-09-21 16:59:38 -0400

    edit code

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,8 @@ const OMDB_SEARCH_API_URL = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=`
       "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
     }
 */
+
+
 let moviesObj = {} 
 
 function createCard(moviesObj) {
@@ -42,31 +44,36 @@ function createCard(moviesObj) {
 function searchMovies(event) {
     console.log(event.code);
     if (event.code === 'Enter') { // input search enter key press
-        const searchInput = document.getElementById('search-input');
-        if (searchInput.value === '') return; // if input empty, do nothing
+        const searchInput = event.target; // Get the current search input element
+
+        if (searchInput.value === '') return; // If input is empty, do nothing
+
         const searchWord = searchInput.value;
         // search
-        fetch(OMDB_SEARCH_API_URL+searchWord)
-        .then(response=>response.json())
-        .then(data=>{
-            //show movies cards to moviesDiv
-            showDivMovies(true);
-            showDivIndex(false);
-            showDivMovieDetail(false);
-            //if search result is empty
-            const moviesDiv = document.getElementById('movies');
-            if (data.Search.length === 0) {
-                //show nothing found
-                moviesDiv.innerHTML = "No related result found"
-            }
-            
-            for (let movie of data.Search) {
-                moviesDiv.appendChild(createCard(movie))
-            }
-        })
-        }
-}
+        fetch(OMDB_SEARCH_API_URL + searchWord)
+            .then(response => response.json())
+            .then(data => {
+                // Show movies cards to moviesDiv
+                showDivMovies(true);
+                showDivIndex(false);
+                showDivMovieDetail(false);
+                // If search result is empty
+                const moviesDiv = document.getElementById('movies');
+                if (data.Search.length === 0) {
+                    // Show "No related result found"
+                    moviesDiv.innerHTML = "No related result found"
+                } else {
+                    // Clear previous search results
+                    moviesDiv.innerHTML = "";
 
+                    // Append search results to moviesDiv
+                    for (let movie of data.Search) {
+                        moviesDiv.appendChild(createCard(movie));
+                    }
+                }
+            });
+    }
+}
 function showDivIndex(display) {
     const divIndex = document.getElementById('index');
     if (display) {
@@ -96,3 +103,6 @@ function showDivMovieDetail(display) {
 
 const searchInput = document.getElementById('search-input');
 searchInput.addEventListener('keydown', searchMovies)
+
+const searchInput2 = document.getElementById('search-input2');
+searchInput2.addEventListener('keydown', searchMovies)
